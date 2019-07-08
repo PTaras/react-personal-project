abe8fe997df3e35ae72b7d4c2dc1f6df01c1130d
PTaras <potsikaylot@gmail.com>
2019-07-08 20:26:18 +0300

    peraonal_project

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -1,15 +1,105 @@
 // Core
 import React, { Component } from 'react';
+import { v4 } from 'uuid';
+import moment from 'moment';
+
+//Components
+import { withProfile } from 'components/HOC/withProfile';
+import Spinner from 'components/Spinner';
+import Task from '../Task';
+import Checkbox from 'theme/assets/Checkbox';
 
 // Instruments
 import Styles from './styles.m.css';
+import { delay } from 'instruments';
 import { api } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
-
+@withProfile
 export default class Scheduler extends Component {
+
+    state = {
+         tasks: [],
+         message: '',
+        };
+
+        _setTaskSpinningState = (state) => {
+        this.setState({
+            isTaskSpinning: state,
+        });
+    }
+
+    _createTask (message) {
+        this._setTaskSpinningState(true) 
+
+        const task = {
+            id:          v4(),
+            completed:  false,
+            favorite:   false,
+            message,
+        };
+
+        this.setState(({ tasks }) => ({
+            tasks: [task, ...tasks],
+            isPostSpinning: false,
+        }));
+    }
+
+    _updateMessage (event) {
+        this.setState({
+            message: event.target.value,
+        });
+    }
+
+    _submitMessage (event) {
+        event.preventDefault();
+        const { message } = this.state;
+
+        if (!message) {
+            return null;
+        }
+
+        this.props._createTask(message);
+
+        this.setState({
+            message: '',
+        });
+    }
+
     render () {
+
+        const { tasks, isTaskSpinning } = this.state;
+
+        const tasksJSX = tasks.map((task) => {
+            return <Task key = { task.id } { ...task } />
+        })
+
         return (
             <section className = { Styles.scheduler }>
-                Планировщик: стартовая точка
+                <Spinner isSpinning = { isTaskSpinning } />
+                <main>
+                    <header>
+                        <h1>Планировщик задач</h1>
+                        <input placeholder="Find" type="Search"/>
+                    </header>
+                    <section>
+                        <form onSubmit = { this._submitMessage }>
+                            <input  onChange = { this._updateMessage }  maxLength = "50" placeholder = "Description new task" type = "text"></input>
+                            <button>Add task</button>
+                        </form>
+                        <div>
+                            <ul>
+                                 <div>
+                                    <Task />
+                                    {tasksJSX}
+                                </div>
+                            </ul>
+                        </div>
+                    </section>
+                        {tasksJSX}
+                    <footer>
+                        <div><Checkbox /></div>
+                        <span className = { Styles.completeAllTasks }>All tasks complited!</span>
+                    </footer>
+                </main>
             </section>
         );
     }
--- source/components/Task/index.js
+++ source/components/Task/index.js
@@ -1,9 +1,16 @@
 // Core
 import React, { PureComponent } from 'react';
 
+// Components
+import { withProfile } from 'components/HOC/withProfile';
+
 // Instruments
 import Styles from './styles.m.css';
-
+import Checkbox from 'theme/assets/Checkbox';
+import Edit from 'theme/assets/Edit';
+import Remove from 'theme/assets/Remove';
+import Star from 'theme/assets/Star';
+@withProfile
 export default class Task extends PureComponent {
     _getTaskShape = ({
         id = this.props.id,
@@ -16,8 +23,27 @@ export default class Task extends PureComponent {
         favorite,
         message,
     });
-
+    
+    state = {
+        message: '',
+    };
     render () {
-        return <li className = { Styles.task }>Задача: стартовая точка</li>;
+
+        const { message } = this.state;
+    
+        return (
+            <li className = { Styles.task }>
+            <div className = { Styles.content }>
+                <div className = { Styles.toggleTaskCompletedState }><Checkbox />
+                    </div>
+                    <input disabled maxLength = "50" type = "text" value = { message } />
+                </div>
+            <div className = { Styles.actions }>
+                <div className = { Styles.toggleTaskFavoriteState } ><Star /></div>
+                <div className = { Styles.updateTaskMessageOnClick }><Edit /></div>
+                <Remove />
+            </div>
+        </li>
+        );
     }
 }
